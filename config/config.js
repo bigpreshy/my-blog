2dd7fa6d5e0f61392848713a416b74a258d43fc2
Preshy <lukeprecious@gmail.com>
2025-03-24 18:36:45 +0100

    Updated Config

---
+++ config/config.js
@@ -0,0 +1,32 @@
+
+
+require('dotenv').config(); // Load environment variables from .env
+
+module.exports = {
+  development: {
+   
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD ,
+    database: process.env.DB_NAME_TEST ,
+    host: process.env.DB_HOST ,
+    dialect: "mysql",
+    port: process.env.DB_PORT ,
+    timezone: process.env.DB_TIMEZONE || "+01:00"
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD ,
+    database: process.env.DB_NAME_TEST ,
+    host: process.env.DB_HOST ,
+    dialect: "mysql"
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD ,
+    database: process.env.DB_NAME_TEST ,
+    host: process.env.DB_HOST ,
+    dialect: "mysql",
+    port: process.env.DB_PORT ,
+    timezone: process.env.DB_TIMEZONE || "+01:00"
+  }
+};
